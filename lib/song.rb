bc787f834f8fc05e35c74b44cc7967b53f9080c9
Priyanka-Shahi <shahipriyan@gmail.com>
2022-09-13 12:56:37 +1000

    commit tree 82d3b27be26f23c9d926aa92ce59399431e82eb5

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,21 @@ class Song
   def save
     self.class.all << self
   end
-
 end
+
+def self.create 
+  song = Song.new
+  song.save
+end
+
+def self.create_by_name(name)
+  song = Song.new
+  song.name = name
+  song.save
+end
+
+def find_by_name(name)
+  @@all.find |song|
+  song.name == name
+end
+
